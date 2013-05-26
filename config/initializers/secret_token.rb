d65f826240d063a25d930b9e40ddf69c0c26b003
Lars Breuer <lars.breuer@fivestrikes.de>
2013-05-26 16:47:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiveStrikes2::Application.config.secret_token = '603c67e79e43c3a364b8925f0b8904702522665f34d2c2ad82cf9a5146b6ee2495b06b41be7e025c7a293d9f208d339298cb85fb37601b47c597bc00b960d710'
