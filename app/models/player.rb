7890fa1eb363329ff2fdc2f35ae7abcbb1081595
Lars Breuer <lars.breuer@fivestrikes.de>
2013-12-08 10:54:34 +0100

    Search Players 06.12.2013

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,15 @@ class Player < ActiveRecord::Base
 	has_many :tickers, :dependent => :destroy
 	has_many :games, :through => :tickers
 	
+	def self.search(team_id, player_name)
+		if team_id
+			if player_name
+				find(:all, :conditions => ['team_id LIKE ? AND player_name LIKE ?', "%#{team_id}%", "%#{player_name}%"])
+			else
+				find(:all, :conditions => ['team_id LIKE ?', "%#{team_id}%"])
+			end
+		else
+    		find(:all)
+  		end
+	end
 end
