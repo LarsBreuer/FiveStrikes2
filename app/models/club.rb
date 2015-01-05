21f29a630d1460b70bf2643aba5078b52e4ab4ed
LarsBreuer <lars-breuer@gmx.de>
2015-01-05 10:23:39 +0100

    Änderungen Club 05.01.2014

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,4 +2,12 @@ class Club < ActiveRecord::Base
 
 	has_many :teams
 
+	def self.search(search)
+		if search
+			find(:all, :conditions => ['club_name LIKE ?', "%#{search}%"])
+		else
+    		find(:all)
+  		end
+	end
+
 end
