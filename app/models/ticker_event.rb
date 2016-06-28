a8cd8f11b055a62075220c43b5fd7a0cee66e4bb
LarsBreuer <lars-breuer@gmx.de>
2016-06-28 09:12:52 +0200

    Ticker einrichten I 28.06.2016

--- app/models/ticker_event.rb
+++ app/models/ticker_event.rb
@@ -1,6 +1,12 @@
 class TickerEvent < ActiveRecord::Base
-  attr_accessible :game_id, :ticker_event_note, :time
+  	attr_accessible :game_id, :ticker_event_note, :time
 
-  belongs_to :game
+  	belongs_to :game
+
+  	def self.convert_seconds_to_time(seconds)
+		total_minutes = seconds / 1.minutes
+		seconds_in_last_minute = seconds - total_minutes.minutes.seconds
+		"#{sprintf '%02d', total_minutes}:#{sprintf '%02d', seconds_in_last_minute}"
+	end
   
 end
