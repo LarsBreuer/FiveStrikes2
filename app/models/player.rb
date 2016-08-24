c47d5ef1dc5f1585595748f768cbc5bc074020ae
Oliver Kiessler <kiessler@inceedo.com>
2016-08-24 17:28:37 +0200

    Issue #4

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,12 +1,11 @@
-class Player < ActiveRecord::Base
-	require 'csv'
-  #require 'iconv'
+require 'csv'
 
+class Player < ActiveRecord::Base
 	belongs_to :team
 	has_many :ticker_activities, :dependent => :destroy
 	has_many :games, :through => :ticker_activities
 	has_many :line_items
-	
+
 	def self.search(team_id, player_forename, player_surename)
 		if team_id
 			if player_forename && player_surename
@@ -21,20 +20,16 @@ class Player < ActiveRecord::Base
 
 	# Import CSV-File
 	def self.import(file_in, team_id)
-
-    # Datei umwandeln wegen Umlaute (ä,ü,ö)
-    #logger.debug "Datei umwandeln"
-    #file = Iconv.conv("UTF8", "LATIN3", file_in)
-
-    # Daten aus der CSV herauslesen
-    CSV.foreach(file.path, headers: true) do |row|
-
-    	player_hash = row.to_hash 
-    	player = Player.where(player_forename: player_hash["player_forename"], player_surename: player_hash["player_surename"], team_id: team_id)
-    	if player.count == 0
-     		Player.create!(player_hash.merge({ team_id: team_id }))
-    	end
-    end
+		if (team = Team.find(team_id))
+			# Daten aus der CSV herauslesen
+			CSV.foreach(file_in.path, headers: true, encoding:'iso-8859-1:utf-8') do |row|
+				player_hash = row.to_hash
+				player_hash.keys.each {|k| player_hash[k] = player_hash[k].encode("iso-8859-1").force_encoding("utf-8")}
+				puts "CSV row: #{player_hash.inspect}"
+				players = Player.where(player_forename: player_hash["player_forename"], player_surename: player_hash["player_surename"], team_id: team_id)
+				team.players.create(player_hash) if players.count == 0
+			end
+		end
   end
 
   def get_player_position_by_id(position_id)
@@ -63,7 +58,7 @@ class Player < ActiveRecord::Base
   end
 
   def get_player_games
-    
+
     game_array = Array.new
 
     self.games.each do |game|
@@ -75,5 +70,5 @@ class Player < ActiveRecord::Base
     return game_array
 
   end
-	
+
 end
