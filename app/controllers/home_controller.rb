515330e55ce69efe81da7587ce8900285baeed8c
LarsBreuer <lars-breuer@gmx.de>
2017-05-09 09:55:54 +0200

    Änderung der Darstellung Spiele FIVES II 09.05.2017

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -281,7 +281,7 @@ class HomeController < ApplicationController
   def check_if_friend
     @game = Game.find(params[:game_id])
     @user = User.find(@game.user_id)
-    redirect_to fb_ask_friend_path(:id => @game.user_id), :remote => true, notice: 'Du bist kein Freund' unless current_user.friend.include?(@user) || current_user == @user || current_user.name == 'JaqenHghar' || @user.name == 'larsb'
+    redirect_to fb_ask_friend_path(:id => @game.user_id), :remote => true, notice: 'Du bist kein Freund' unless current_user.friend.include?(@user) || current_user == @user || current_user.name == 'JaqenHghar' || @user.name == 'FIVES'
   end
 
   def authenticate_user!
